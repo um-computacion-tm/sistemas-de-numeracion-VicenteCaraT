80993b817636119221dd34dd06212ec7346a1de6
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2023-04-17 23:55:48 -0300

    Código decimal2roman y roman2decimal completos

--- function_numeracion.py
+++ function_numeracion.py
@@ -1,8 +1,20 @@
 def binario2decimal (binario):
-    decimal = int(binario, 2)
-    return decimal
+    total = 0
+    for i in range(0, len(binario)):
+        total += int(binario[-i-1]) + 2 **i
+    return total
 
-
+def decimal2binario (decimal):
+    if decimal == 0:
+        return '0'
+    digitos_bin = []
+    while decimal > 0:
+        resto = decimal % 2
+        digitos_bin.append(str(resto))
+        decimal //=2
+    resultado = ''.join(digitos_bin[::-1])
+    return resultado
+    
 
 if __name__ == '__main__':
     pass
--- my_test_numeracion.py
+++ my_test_numeracion.py
@@ -1,13 +1,13 @@
 import unittest
 from function_numeracion import binario2decimal
-
+from function_numeracion import decimal2binario
 
 class TestNumeracion (unittest.TestCase):
     def binario2decimal(self):
         resultado = (binario2decimal('01011100', 92))
         self.assertEqual(resultado)
-#    def decimal2binario(self):
-#        self.assertEqual (decimal2binario(97, '01100001'))
+    def decimal2binario(self):
+        self.assertEqual (decimal2binario(97, '01100001'))
 
 
 if __name__ == '__main__':
